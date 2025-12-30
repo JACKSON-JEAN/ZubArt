4e1c1678fb03f1b3b9983c5772eadf0c75251a7d
JACKSON-JEAN <jacksonuwizeye@gmail.com>
2025-12-30 17:03:38 +0300

    Stripe v2

--- prisma/migrations/20251230135809_add_payment_transaction_id_uuid/migration.sql
+++ prisma/migrations/20251230135809_add_payment_transaction_id_uuid/migration.sql
@@ -2,7 +2,7 @@
   Warnings:
 
   - A unique constraint covering the columns `[transactionId]` on the table `Payment` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `transactionId` to the `Payment` table without a default value. This is not possible if the table is not empty.
+  - The required column `transactionId` was added to the `Payment` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
 
 */
 -- AlterTable
